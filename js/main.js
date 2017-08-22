0c386494e07c1f4db71b017a761985e9a2b87a47
Michael Harrington <mike85h@gmail.com>
2017-08-22 16:17:46 -0600

    front end get added and all books heading and info appended to body. all working and getting from heroku

--- js/main.js
+++ js/main.js
@@ -1,1 +1,22 @@
-console.log("working");
+const prodURL = 'https://stormy-ocean-79229.herokuapp.com/'
+const devURL = 'http://localhost:8080/'
+
+function getAllBooks(){
+  $.get(prodURL, function(data){
+    $('body').append(
+      `<h1>All Books</h1>`
+    )
+    for(let i = 0; i < data.length; i++){
+      $('body').append(
+        `<div id=${data[i].title}>
+          ${data[i].title}<br>
+          ${data[i].genre}<br>
+          ${data[i].description}<br>
+          ${data[i].coverURL}<br><br>
+        </div>`
+      )
+    }
+  })
+}
+
+getAllBooks();
